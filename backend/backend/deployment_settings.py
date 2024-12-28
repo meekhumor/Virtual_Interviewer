8b12f4fd8c4d7661aea325a5a2b180b68b24703e
meekhumor <om17092004@gmail.com>
2024-12-28 15:23:11 +0530

    fix: update environment variable access syntax in deployment settings

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -3,12 +3,12 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [[os.environ.get['RENDER_EXTERNAL_HOSTNAME']]]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -22,21 +22,18 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-
-# CORS_ALLOW_ALL_ORIGINS 
-
 STORAGES = {
     "default": {
-        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
 DATABASES = {
     'default': dj_database_url.config(
-        default= os.environ['DATABASE_URL'], 
+        default=os.environ.get('DATABASE_URL'), 
         conn_max_age=600
     )
 }
